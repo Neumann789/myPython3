762b20e9f69793453f17a93cb958b33574a5239d
fhb <test@test.com>
2018-03-09 10:27:44 +0800

    练习数字类型

---
+++ learn/tkinter/testTkinter.py
@@ -0,0 +1,11 @@
+__author__ = 'fanghuabao'
+
+from tkinter import *
+
+if __name__ == '__main__':
+    root = Tk()
+    label = Label(root, text='Hello World')
+    label.pack()
+    root.mainloop()
+
+
---
+++ learn/wxWidgets/testWxWidgets.py
@@ -0,0 +1,5 @@
+__author__ = 'fanghuabao'
+
+# import wx wx µÄ¿âÕÒ²»µ½
+# class Frame(wx.Frame):
+#     pass
